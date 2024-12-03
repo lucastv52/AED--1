17a7e9be990772de2f92812567c6fe29ffd20654
Lucas Tosetti de Vasconcellos <91163133+lucastv52@users.noreply.github.com>
2024-12-03 15:11:25 -0300

    Update 1340(incompleto).c

--- 1340(incompleto).c
+++ 1340(incompleto).c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int verificaFila()
 
 
 int main(){
-    int n, x,y;
+    int n, x,y, t, s;
     int pilha, fila, heap = 0;
     
     while(pilha != 20){
+        t, t1,t2,s,s1,s2 = 0;
         scanf("%d", &n);
         for(int i = 0; i<n; i++){
             int pilhas[n], filas[n], heaps[n], saida[n];
             scanf("%d %d", &x, &y);
             if(x == 1){
-                pilhas[i] = y;
-                filas[i] = y;
-                heaps[i] = y;
+                filas[t++] = y;
+                pilhas[t1++] = y;
+                
             }else if(x == 2)
                 saida[i] = y;
 
