bd2a20d16b57bdba45e91f8c8d97841d40f84ffa
Lucas Tosetti de Vasconcellos <91163133+lucastv52@users.noreply.github.com>
2025-01-14 17:33:10 -0300

    Update 1855(incompleto).c

--- 1855(incompleto).c
+++ 1855(incompleto).c
@@ -9,15 +9,54 @@ int main(){
     char entrada;
     for(int i = 0; i<linha; i++){
         for(int j = 0; j<coluna; j++){
-            scanf("%c", &entrada);
+            scanf(" %c", &entrada);
             matriz[i][j] = entrada;
         }
     }
-    // for(int i = 0; i<linha; i++){
-    //     for(int j = 0; j<coluna; j++){
-    //         printf("%c", matriz[i][j]);
-    //     }
-    // }
+    int playerX, playerY, count;
+    playerY = 0;
+    playerX = 0;
+    char mark, valid;
+    mark = matriz[0][0];
+    valid  = '!';
+    count = 0;
+    while(playerY >= 0 && playerY < linha && playerX >= 0 && playerX < coluna && count <= linha * coluna){
+        
+        switch(mark){
+            case '>':
+                playerX++;
+                if(matriz[playerY][playerX] != '.' && matriz[playerY][playerX] != '*')
+                    mark = matriz[playerY][playerX];
+                if(matriz[playerY][playerX] == '*')
+                    valid ='*';
+                break;
+            case '<':
+                playerX--;
+                if(matriz[playerY][playerX] != '.' && matriz[playerY][playerX] !='*')
+                    mark = matriz[playerY][playerX];
+                if(matriz[playerY][playerX] =='*')
+                    valid ='*';
+                break;
+            case '^':
+                playerY--;
+                if(matriz[playerY][playerX] != '.' && matriz[playerY][playerX] !='*')
+                    mark = matriz[playerY][playerX];
+                if(matriz[playerY][playerX] =='*')
+                    valid ='*';
+                break;
+            case 'v':
+                playerY++;
+                if(matriz[playerY][playerX] != '.' && matriz[playerY][playerX] !='*')
+                    mark = matriz[playerY][playerX];
+                if(matriz[playerY][playerX] =='*')
+                    valid ='*';
+                break;
 
+        }
+        if(valid == '*')
+            break;
+        count++;
+    }
+    printf("%c\n", valid);
     return 0;
 }
